3641222eea05a0702a5b949c717638f0d792badb
Laksa Bayu <laksabayu18.com>
2024-01-22 00:28:57 +0700

    change code AuthJWTMiddleware

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -7,66 +7,42 @@ import (
 	"projectsistemkasir/helper"
 	"projectsistemkasir/model/web"
 	"strings"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-type AuthMiddleware struct {
-	Handler http.Handler
-}
+func AuthJWTMiddleware(next httprouter.Handle) httprouter.Handle {
+	return func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		//logika jwt
+		writer.Header().Set("Content-Type", "application/json")
+		token := request.Header.Get("Authorization")
 
-func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
-	return &AuthMiddleware{Handler: handler}
-}
-
-func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	// if "PINJAM_DULU_SERATUS" == request.Header.Get("Authorization") {
-		
-	// 	middleware.Handler.ServeHTTP(writer, request) //Diteruskan ke handler selanjutnya
-
-	// } else {
-	// 	// error
-	// 	writer.Header().Set("Content-Type", "application/json")
-	// 	writer.WriteHeader(http.StatusUnauthorized)
-
-	// 	webResponse := web.WebResponse {
-	// 		Code: http.StatusUnauthorized,
-	// 		Status: "UNAUTHORIZED",
-	// 	}
-
-	// 	helper.WriteToResponseBody(writer, webResponse)
-		
-	// }
-
-
-	writer.Header().Set("Content-Type", "application/json")
-	token := request.Header.Get("Authorization")
-
-	if token == "" {
-		writer.WriteHeader(http.StatusUnauthorized)
-		webResponse := web.WebResponse {
-			Code: http.StatusUnauthorized,
-			Status: "UNAUTHORIZED",
+		if token == "" {
+			writer.WriteHeader(http.StatusUnauthorized)
+			webResponse := web.WebResponse {
+				Code: http.StatusUnauthorized,
+				Status: "UNAUTHORIZED",
+			}
+			json.NewEncoder(writer).Encode(webResponse)
+			return
 		}
-		json.NewEncoder(writer).Encode(webResponse)
-		return
-	}
 
-	token = strings.Split(token, " ")[1]
-	claims, err := helper.DecodeToken(token)
+		token = strings.Split(token, " ")[1]
+		claims, err := helper.DecodeToken(token)
 
-	if err != nil {
-		writer.WriteHeader(http.StatusUnauthorized)
-		webResponse := web.WebResponse {
-			Code: http.StatusUnauthorized,
-			Status: "UNAUTHORIZED",
+		if err != nil {
+			writer.WriteHeader(http.StatusUnauthorized)
+			webResponse := web.WebResponse {
+				Code: http.StatusUnauthorized,
+				Status: "UNAUTHORIZED",
+			}
+			json.NewEncoder(writer).Encode(webResponse)
+			return
 		}
-		json.NewEncoder(writer).Encode(webResponse)
-		return
+
+		ctx := context.WithValue(request.Context(), "userId", claims)
+		request = request.WithContext(ctx)
+
+		next(writer,request,params)
 	}
-
-	ctx := context.WithValue(request.Context(), "userInfo", claims)
-	request = request.WithContext(ctx)
-	middleware.Handler.ServeHTTP(writer, request.WithContext(ctx))
-
 }
-
-//cara implementasi middleware di golang (middleware untuk jwt auth)
